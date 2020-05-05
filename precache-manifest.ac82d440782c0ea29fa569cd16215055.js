5f5d7bbf546d9cda152adf5b8c69b81071b9bde7
maxim.lavrinenko <Maxim.Lavrinenko@norbit.ru>
2020-05-05 12:26:55 +0300

    Updates

---
+++ precache-manifest.ac82d440782c0ea29fa569cd16215055.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a2545fe3a42d8cae346afab48491690",
+    "url": "/covid-info/index.html"
+  },
+  {
+    "revision": "241e4c5a8192252e8775",
+    "url": "/covid-info/static/css/main.3642621f.chunk.css"
+  },
+  {
+    "revision": "12afa853c8515cc88654",
+    "url": "/covid-info/static/js/2.2f8fe134.chunk.js"
+  },
+  {
+    "revision": "6dcd373a94606ea12253d28720535d4c",
+    "url": "/covid-info/static/js/2.2f8fe134.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "241e4c5a8192252e8775",
+    "url": "/covid-info/static/js/main.ee84e899.chunk.js"
+  },
+  {
+    "revision": "84510d224526028776f4",
+    "url": "/covid-info/static/js/runtime-main.5ebd7329.js"
+  },
+  {
+    "revision": "197c0ffb448001491b0ab0101179f5ff",
+    "url": "/covid-info/static/media/bg.197c0ffb.png"
+  },
+  {
+    "revision": "52bd98c78d2acfbafa3f776f6fab3330",
+    "url": "/covid-info/static/media/country-card.52bd98c7.jpg"
+  },
+  {
+    "revision": "7efd109b25fc286b5e11533dc2b1a740",
+    "url": "/covid-info/static/media/spinner.7efd109b.gif"
+  }
+]);
